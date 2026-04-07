cd11d98ed98979813327aaaf912384c3159b98d7
Jesús Iván Lemus Cervantes <thejesus567@gmail.com>
2026-04-06 18:57:50 -0600

    added infra

--- infra/s3.tf
+++ infra/s3.tf
@@ -2,15 +2,40 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "my-s3-bucket-05042026"
-  
+
   website = {
-    "index_document": "index.html",
+    "index_document" : "index.html",
   }
 
-  attach_public_policy = true
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.s3-policy-document.json
+
   restrict_public_buckets = false
 
-   versioning = {
+  versioning = {
     enabled = true
   }
+}
+
+// Bucket policy 
+
+data "aws_iam_policy_document" "s3-policy-document" {
+  statement {
+    sid = "PublicReadGetObject"
+    effect = "Allow"
+
+    actions = ["s3:GetObject"]
+
+    resources = ["arn:aws:s3:::my-s3-bucket-05042026/*"]
+    
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+
+output "policy" {
+  value = data.aws_iam_policy_document.s3-policy-document.json
 }
